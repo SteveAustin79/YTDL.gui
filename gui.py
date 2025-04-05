b52b176a1333047ae355dc7c842b1cd9cd7ccad6
SteveAustin79 <steveaustin79@proton.me>
2025-04-06 01:49:34 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -1390,7 +1390,7 @@ app.configure(bg_color=COLORS.black)
 app.protocol("WM_DELETE_WINDOW", on_closing)
 app.grid_columnconfigure(0, minsize=250)
 app.grid_columnconfigure(1, minsize=300)
-app.grid_columnconfigure(2, minsize=440)
+app.grid_columnconfigure(2, minsize=470)
 app.grid_columnconfigure(3, minsize=240)
 
 # Add UI elements
