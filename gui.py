604cefdcc20cf49448527385b30cc391571be4a4
SteveAustin79 <steveaustin79@proton.me>
2025-04-05 22:15:31 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -940,18 +940,18 @@ def limit_resolution(resolution: str, limit: str) -> str:
     return max_resolution
 
 
-# def loop_download(audio_or_video_bool, default_max_res, default_filter_words, only_restricted_videos_bool,
-#                         skip_restricted_bool, year_subfolders, min_duration_bool, min_duration, max_duration_bool, max_duration,
-#                         min_year, max_year, min_video_views):
-#     disable_buttons()
-#     t_loop_download = threading.Thread(target=lambda: loop_download_work(audio_or_video_bool, default_max_res, default_filter_words, only_restricted_videos_bool,
-#                         skip_restricted_bool, year_subfolders, min_duration_bool, min_duration, max_duration_bool, max_duration,
-#                         min_year, max_year, min_video_views), daemon=True)
-#     t_loop_download.start()
-#     t_loop_download.join()
-
-
 def loop_download(audio_or_video_bool, default_max_res, default_filter_words, only_restricted_videos_bool,
+                        skip_restricted_bool, year_subfolders, min_duration_bool, min_duration, max_duration_bool, max_duration,
+                        min_year, max_year, min_video_views):
+    disable_buttons()
+    t_loop_download = threading.Thread(target=lambda: loop_download_work(audio_or_video_bool, default_max_res, default_filter_words, only_restricted_videos_bool,
+                        skip_restricted_bool, year_subfolders, min_duration_bool, min_duration, max_duration_bool, max_duration,
+                        min_year, max_year, min_video_views), daemon=True)
+    t_loop_download.start()
+    t_loop_download.join()
+
+
+def loop_download_work(audio_or_video_bool, default_max_res, default_filter_words, only_restricted_videos_bool,
                         skip_restricted_bool, year_subfolders, min_duration_bool, min_duration, max_duration_bool, max_duration,
                         min_year, max_year, min_video_views):
     count_restricted_videos = 0
