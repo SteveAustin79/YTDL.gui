9a6f7accfee30dcd95efddb66f6a9b7c4c2e2dcc
SteveAustin79 <steveaustin79@proton.me>
2025-04-05 18:20:20 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -296,6 +296,7 @@ def rename_files_in_temp_directory() -> None:
     for filename in os.listdir(directory):
         if ":" in filename:  # Check if filename contains ':'
             sanitized_name = filename.replace(":", "")
+            sanitized_name = clean_string_regex(sanitized_name)
             old_path = os.path.join(directory, filename)
             new_path = os.path.join(directory, sanitized_name)
             os.rename(old_path, new_path)
