03464169723d92bdd4ab645d2620139530921d20
SteveAustin79 <steveaustin79@proton.me>
2025-04-05 19:15:32 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -386,8 +386,7 @@ def enable_buttons():
 
 
 def get_information():
-    get_information_button.grid_remove()
-    separator1.update()
+    disable_buttons()
     threading.Thread(target=get_information_work, daemon=True).start()
 
 
@@ -886,7 +885,7 @@ def get_information_work():
         video_button.grid_remove()
         audio_button.grid_remove()
 
-    get_information_button.grid(row=1, column=3, padx=padding_x, pady=padding_y, sticky="w")
+    enable_buttons()
 
 
 def print_resolutions(yt: YouTube) -> list[str]:
