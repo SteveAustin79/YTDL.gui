91f123e7320989286b4839f70f925cb86a69464b
SteveAustin79 <steveaustin79@proton.me>
2025-04-06 02:07:17 +0200

    updated YTDL.gui

--- main.py
+++ main.py
@@ -366,7 +366,7 @@ def list_channels_work():
             elements_to_destroy.append(label_last_updated)
 
         row_factor += 1
-        separator_label = customtkinter.CTkFrame(app, height=1, fg_color=COLORS.separator)
+        separator_label = customtkinter.CTkFrame(app, height=2, fg_color=COLORS.separator)
         separator_label.grid(row=i + row_factor, column=0, columnspan=18, sticky="ew", padx=padding_x, pady=padding_y)
         elements_to_destroy.append(separator_label)
 
