77553637e43ed20b9ae7fa977d13ffe52b62e09d
SteveAustin79 <steveaustin79@proton.me>
2025-04-08 17:47:32 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -1025,7 +1025,7 @@ def loop_download_work(audio_or_video_bool, default_max_res, default_filter_word
             if default_filter_words == "" or any(
                     word.lower() in video.title.lower() for word in string_to_list(default_filter_words)):
                 print(default_filter_words)
-                if len(string_to_list(default_filter_words)) > 0:
+                if len(string_to_list(default_filter_words)) > 1:
                     configuration_filter_words.configure(fg_color=COLORS.dark_green)
                 if min_duration_bool:
                     video_duration = int(video.length)
