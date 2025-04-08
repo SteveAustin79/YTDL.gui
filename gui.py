dcbc0d3e44a0daa91d679ce09d02d75c229a8268
SteveAustin79 <steveaustin79@proton.me>
2025-04-08 23:49:38 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -995,7 +995,9 @@ def loop_download_work(audio_or_video_bool, default_max_res, default_filter_word
     count_this_run = 0
     count_skipped = 0
     v_counter = 0
-    skip_count = int(skip_videos.get())
+    skip_count = 0
+    if skip_videos.get() !="":
+        skip_count = int(skip_videos.get())
 
     global video_watch_urls
     global elements_to_destroy_loop
