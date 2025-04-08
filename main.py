3dbd8e64ec3f3c2ed5c451113cf63c1360bb0a56
SteveAustin79 <steveaustin79@proton.me>
2025-04-08 19:07:06 +0200

    updated YTDL.gui

--- main.py
+++ main.py
@@ -369,7 +369,8 @@ def list_channels_work():
                                                               text_color=color_else, height=row_height)
             label_latest_video_title.grid(row=i + row_factor, column=17, padx=padding_x, pady=padding_y, sticky="w")
             elements_to_destroy.append(label_latest_video_title)
-            Tooltip(label_latest_video_title, latest_video_title_text)
+            if len(str(latest_video_title_text)) > title_width:
+                Tooltip(label_latest_video_title, latest_video_title_text)
 
             label_video_count.configure(text_color=color_else)
             if counter_color != COLORS.gray:
