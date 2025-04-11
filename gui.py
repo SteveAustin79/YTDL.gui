2e1f8894059bcd6c01f0285ce000b25f8a9f9a33
SteveAustin79 <steveaustin79@proton.me>
2025-04-12 01:50:33 +0200

    updated YTDL.gui

--- gui.py
+++ gui.py
@@ -846,8 +846,8 @@ def get_information_work():
     # video_math.grid(row=5, column=1, padx=PADDING_X, pady=PADDING_Y, sticky="sw")
     # elements_to_destroy.append(video_math)
 
-    channel_videos_combobox = customtkinter.CTkComboBox(channel_frame, values=video_watch_urls)
-    channel_videos_combobox.grid(row=10, column=0, columnspan=2, padx=PADDING_X, pady=PADDING_Y, sticky="sw")
+    channel_videos_combobox = customtkinter.CTkComboBox(channel_frame, values=video_watch_urls, width=ENTRY_WIDTH)
+    channel_videos_combobox.grid(row=11, column=0, columnspan=2, padx=PADDING_X, pady=PADDING_Y, sticky="sw")
     elements_to_destroy.append(channel_videos_combobox)
 
     channel_frame.grid(row=3, column=0, columnspan=4, sticky="ew", padx=0, pady=PADDING_Y * PADDING_Y_FACTOR)
