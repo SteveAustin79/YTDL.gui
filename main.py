8a87dad14b84bb5f22ecdb782fac51815738d941
SteveAustin79 <steveaustin79@proton.me>
2025-04-13 02:57:51 +0200

    updated YTDL.gui

--- main.py
+++ main.py
@@ -440,7 +440,7 @@ app.configure(bg_color=COLORS.black)
 header_frame = customtkinter.CTkFrame(app, fg_color=COLORS.frame_bg)
 header_frame.grid(row=0, column=0, columnspan=18, padx=0, pady=padding_y, sticky="nw")
 
-main_frame = customtkinter.CTkScrollableFrame(app, height=AppConfig.WIN_HEIGHT - 56, fg_color="#272727")
+main_frame = customtkinter.CTkScrollableFrame(app, height=AppConfig.WIN_HEIGHT - 57, fg_color="#272727")
 main_frame.grid(row=1, column=0, columnspan=18, padx=0, pady=0, sticky="nwe")
 
 logo = customtkinter.CTkImage(light_image=Image.open(AppConfig.LOGO_PATH), size=(60, 40)) # 180x120
