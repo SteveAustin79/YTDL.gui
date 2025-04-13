38c6bfd96a671fc0ddbc14f6e11b864d092b5491
SteveAustin79 <steveaustin79@proton.me>
2025-04-13 02:15:59 +0200

    updated YTDL.gui

--- main.py
+++ main.py
@@ -392,7 +392,7 @@ def list_channels_work():
 
         update_log("")
 
-        separator1.grid(row=row_factor, column=0, columnspan=18, sticky="ew", padx=0, pady=padding_y * padding_y_factor)
+        separator1.grid(row=i + row_factor, column=0, columnspan=18, sticky="ew", padx=0, pady=padding_y * padding_y_factor)
 
     update_app_title()
 
