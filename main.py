262555e3fae05b024467c6415ac0c7a65c6166d0
SteveAustin79 <steveaustin79@proton.me>
2025-04-13 02:41:25 +0200

    updated YTDL.gui

--- main.py
+++ main.py
@@ -289,11 +289,11 @@ def list_channels_work():
                 youtube_vo_age_restricted = youtube_video_object.age_restricted
                 youtube_vo_publish_date = youtube_video_object.publish_date
 
-                update_log("Find match:  " + str(counter) + " / " + str(len(size)) + "     |     " +
-                           str(youtube_vo_publish_date.strftime(AppConfig.DATE_FORMAT_DISPLAY)) + "     |     " +
-                                                    format_time(youtube_vo_length) + "     |     " +
-                                        ("R" if youtube_vo_age_restricted else "_") + "     |     " +
-                                        format_view_count(youtube_vo_views) + "     |     " + youtube_vo_title)
+                update_log("Find match:  " + str(counter) + " / " + str(len(size)) + "    |    " +
+                           str(youtube_vo_publish_date.strftime(AppConfig.DATE_FORMAT_DISPLAY)) + "    |    " +
+                                                    format_time(youtube_vo_length) + "    |    " +
+                                        ("R" if youtube_vo_age_restricted else "_") + "    |    " +
+                                        format_view_count(youtube_vo_views) + "    |    " + youtube_vo_title)
 
                 if (youtube_vo_vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
                         youtube_vo_vid_info.get('playabilityStatus', {}).get(
